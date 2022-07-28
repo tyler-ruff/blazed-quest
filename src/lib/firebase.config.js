858c9ffb4195344920a0688e8454a269760e1a2e
Tyler Ruff <hello@blazed.space>
2022-07-27 20:32:28 -0400

    BLZ

---
+++ src/lib/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDJNpSgZqhn1_ipvk54wsU11UIE1BoYIfc",
+    authDomain: "blz-one-9e383.firebaseapp.com",
+    projectId: "blz-one-9e383",
+    storageBucket: "blz-one-9e383.appspot.com",
+    messagingSenderId: "817319905653",
+    appId: "1:817319905653:web:8ee0f28da3fc5e208bab3c",
+    measurementId: "G-1194VR7QTN"
+};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,34 +17,54 @@
 	}
 </style>
 
-<section class="bg-white dark:bg-gray-900">
-	<div class="container grid grid-cols-1 gap-8 px-4 py-12 mx-auto lg:grid-cols-2">
-		<div class="flex flex-col items-center max-w-lg mx-auto text-center">
-			<h2 class="text-3xl font-semibold tracking-tight text-gray-800 dark:text-white">
-				Web development
-			</h2>
-
-			<p class="mt-3 text-gray-500 dark:text-gray-300">
-				First class web development services, using a variety of tools and platforms. No matter who your audience is, and what your budget is, we can craft the front-page that will keep them hooked.
-			</p>
-			<br />
-			<a href="https://blazed.contact/website" class="inline-flex items-center justify-center w-full px-5 py-2 mt-6 text-white bg-blue-600 rounded-lg sm:w-auto hover:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-				Contact Us
-			</a>
-		</div>
-
-		<div class="flex flex-col items-center max-w-lg mx-auto text-center">
-			<h2 class="text-3xl font-semibold tracking-tight text-gray-800 dark:text-white">
-				App development
-			</h2>
-
-			<p class="mt-3 text-gray-500 dark:text-gray-300">
-				Whether it be mobile app development or desktop development, we will assure your customer's experience is top notch. From flow chart to product testing, every stage is covered by our full package.
-			</p>
-			
-			<a href="https://blazed.contact/app" class="inline-flex items-center justify-center w-full px-5 py-2 mt-6 text-gray-700 transition-colors duration-150 transform bg-white border border-gray-200 rounded-lg dark:bg-gray-900 dark:border-gray-700 hover:bg-gray-100 dark:text-white sm:w-auto dark:hover:bg-gray-800 dark:ring-gray-700 focus:ring focus:ring-gray-200 focus:ring-opacity-80">
-				Contact Us
-			</a>
+<article class="bg-white shadow-md dark:bg-gray-900">
+	<div class="dark:bg-gray-800 dark:text-gray-100 mb-3">
+		<div class="container flex flex-col justify-center p-6 mx-auto lg:flex-row lg:justify-between">
+			<div class="flex items-center justify-center p-6 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+				<img src="https://blazed.sirv.com/logo/john-mcmahon-ljjcoULkxL8-unsplash.jpg" alt="" class="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+			</div>
+			<div class="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+				<h1 class="text-5xl font-bold leading-none sm:text-6xl">Blazed
+					<span class="dark:text-violet-400">Project</span> Management
+				</h1>
+				<p class="mt-6 mb-8 text-lg mt-10 sm:mb-12">Turning dreams into reality
+					<br class="hidden md:inline lg:hidden"> for customers and investors
+					<br class="hidden md:inline lg:hidden"> since 2020.
+				</p>
+				<div class="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+					<div class="inline-flex w-full sm:w-auto sm:mx-2">
+						<a href="/projects" class="inline-flex items-center justify-center w-full px-5 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+							Our Projects
+						</a>
+					</div>
+		
+					<div class="inline-flex w-full mt-4 sm:w-auto sm:mx-2 sm:mt-0">
+						<a href="/about" class="inline-flex items-center justify-center w-full px-5 py-2 text-gray-700 transition-colors duration-150 transform bg-white border border-gray-200 rounded-lg dark:bg-gray-900 dark:border-gray-700 hover:bg-gray-100 dark:text-white sm:w-auto dark:hover:bg-gray-800 dark:ring-gray-700 focus:ring focus:ring-gray-200 focus:ring-opacity-80">
+							Learn More
+						</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
-</section>
+	<hr />
+	<div class="container flex flex-col items-center px-4 py-12 mx-auto text-center">
+		<h2 class="text-2xl font-semibold tracking-tight text-gray-700 sm:text-3xl dark:text-white">
+			Contribute to Our Projects
+		</h2>
+		
+		<div class="mt-6 sm:-mx-2">
+			<div class="inline-flex w-full sm:w-auto sm:mx-2">
+				<a target="_blank" href="https://blazed.dev/" class="inline-flex items-center justify-center w-full px-5 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+					Developers
+				</a>
+			</div>
+
+			<div class="inline-flex w-full mt-4 sm:w-auto sm:mx-2 sm:mt-0">
+				<a href="https://www.facebook.com/groups/blzdev" target="_blank" class="inline-flex items-center justify-center w-full px-5 py-2 text-gray-700 transition-colors duration-150 transform bg-white border border-gray-200 rounded-lg dark:bg-gray-900 dark:border-gray-700 hover:bg-gray-100 dark:text-white sm:w-auto dark:hover:bg-gray-800 dark:ring-gray-700 focus:ring focus:ring-gray-200 focus:ring-opacity-80">
+					Facebook Group
+				</a>
+			</div>
+		</div>
+	</div>
+</article>
