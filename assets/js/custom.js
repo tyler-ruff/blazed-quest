7de629269807b2949b225a40f7e063cfae798b84
Tyler Ruff <hello@blazed.space>
2022-08-25 04:44:11 -0400

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,35 +1,5 @@
 jQuery(document).ready(function($) {
 
-  // Append a close trigger for each block
-  $('.box-tile .content').append('<span class="close">x</span>');
-  // Show window
-  function showContent(elem) {
-    hideContent();
-    elem.find('.content').addClass('expanded');
-    elem.addClass('cover');
-  }
-  // Reset all
-  function hideContent() {
-    $('.box-tile  .content').removeClass('expanded');
-    $('.box-tile  li').removeClass('cover');
-  }
-
-  // When a li is clicked, show its content window and position it above all
-  $('.box-tile  li').click(function() {
-    showContent($(this));
-  });
-  // When tabbing, show its content window using ENTER key
-  $('.box-tile  li').keypress(function(e) {
-    if (e.keyCode == 13) {
-      showContent($(this));
-    }
-  });
-
-  // When right upper close element is clicked  - reset all
-  $('.box-tile  .close').click(function(e) {
-    e.stopPropagation();
-    hideContent();
-  });
   // Also, when ESC key is pressed - reset all
   $(document).keyup(function(e) {
     if (e.keyCode == 27) {
@@ -91,8 +61,8 @@ jQuery(document).ready(function($) {
   });
 
 
-
-  $('iframe').each(function() { /*fix youtube z-index*/
+/*
+  $('iframe').each(function() { 
     var url = $(this).attr("src");
     if (url.indexOf("youtube.com") >= 0) {
       if (url.indexOf("?") >= 0) {
@@ -102,6 +72,7 @@ jQuery(document).ready(function($) {
       }
     }
   });
+*/
 
   $('ul.nav li.dropdown').hover(function() {
     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
