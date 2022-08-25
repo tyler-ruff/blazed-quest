a4f99c752fe982d2153dda52ab1b73c1e01bbdda
Tyler Ruff <hello@blazed.space>
2022-08-25 04:20:12 -0400

    BLZ

--- assets/js/views/portfolio.blz.js
+++ assets/js/views/portfolio.blz.js
@@ -42,6 +42,19 @@ function build_routes(page){
 }
 
 function portfolio(){
+  /**
+      *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+      *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    var disqus_config = function () {
+    this.page.url = window.location.href;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://blazed-quest.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
     return `
      <section id="subintro">
         <div class="jumbotron subhead" id="overview">
