30938167978c1b5da83a9ec01cc0f1b901a4ffee
Tyler Ruff <hello@blazed.space>
2023-06-24 17:53:54 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -59,45 +59,40 @@ export const footerData = {
     {
       title: 'Product',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        { text: 'About', href: getPermalink('/about/') },
+        { text: 'FAQ', href: getPermalink('/faq/') },
+        { text: 'Pricing', href: getPermalink('/pricing/') },
+        { text: 'Blog', href: getBlogPermalink() },
+        { text: 'App', href: 'https://app.blazed.quest/' },
       ],
     },
     {
       title: 'Platform',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
+        { text: 'Blazed One', href: 'https://blz.one/' },
+        { text: 'Community', href: 'https://blazed.city/' },
+        { text: 'Systems', href: 'https://blazed.systems/' },
+        { text: 'Telecom', href: 'https://blazed.tel/' },
       ],
     },
     {
       title: 'Support',
       links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
+        { text: 'Get Support', href: getPermalink('/support/') },
+        { text: 'Docs', href: 'https://blazed.dev/docs/quest/' },
+        { text: 'Community Forum', href: 'https://www.facebook.com/groups/blazedforum' },
+        { text: 'Developers', href: 'https://blazed.dev/' },
+        { text: 'Status', href: 'https://blazed.statuspage.io/' },
       ],
     },
     {
       title: 'Company',
       links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: 'About', href: 'https://blazedlabs.com/about' },
+        { text: 'Blog', href: 'https://blazedlabs.com/blog' },
+        { text: 'Corporate', href: 'https://blazed.company/' },
+        { text: 'Invest', href: 'https://opencollective.com/blazed-dev' },
+        { text: 'Contact', href: 'https://blazed.contact/' },
       ],
     },
   ],
@@ -106,14 +101,14 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'Twitter', icon: 'tabler:brand-twitter', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'Twitter', icon: 'tabler:brand-twitter', href: 'https://twitter.com/BlazedLabs/' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/blazed_labs/' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/blazedlabs' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/blazed-labs' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://blazed.sirv.com/logo/Beaker-Cobalt.png?w=32&h=32)]"></span>
+    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://blazedlabs.com/"> Blazed Labs</a> · All rights reserved.
   `,
 };
